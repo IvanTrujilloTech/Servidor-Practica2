9b6b869ba4d36e63b727b0d40026f12c195ea4ff
Iván Trujillo <156064506+IvanTrujilloTech@users.noreply.github.com>
2025-10-21 17:47:59 +0200

    Add files via upload

---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../sociograma/assets/css/styles.css">
+  <title><?= $page_title ?? 'Sociograma DAW' ?></title>
+</head>
+
+<body>
+  <div class="container">
+    <header class="main-header">
+      <h1>MP07 · Sociograma DAW</h1>
+    </header>
+    <main>
